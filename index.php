2a5bec0408d7290aa256319511a081d6dc1f1222
freerunner1317 <73885411+freerunner1317@users.noreply.github.com>
2021-06-19 19:12:07 +0700

    f

---
+++ index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="/style.css">
+</head>
+	<?
+		$link = mysqli_connect('localhost', 'root', 'root','libraryweb') 
+	    or die("Ошибка " . mysqli_error($link));
+
+	    if(isset($_GET['student'])){
+	    	$query ="INSERT INTO grades (id_subject, id_student, grade, data) VALUES ('{$_GET['subject']}', '{$_GET['student']}', '{$_GET['grade']}', '{$_GET['date']}');
+";	 
+			$result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link));
+	    }
+	?>
+
+<body> 
+	<form method="GET" action="index.php">
+		<div class="grade">
+			<?
+				$query ="SELECT * FROM students;";
+				$result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link)); 
+				foreach ($result as $key => $value) {
+					$students[$key][0] = $value['id'];
+					$students[$key][1] = $value['first_name'];
+					$students[$key][2] = $value['second_name'];
+					$students[$key][3] = $value['group'];
+					$group[$key] = $value['group'];
+					$student[$key] = $value['first_name']." ".$value['second_name'];
+				}
+
+
+			?>
+			<select name="student">	
+				<?
+				    foreach ($students as $key => $value) {
+				    	echo "<option value='$value[0]'>$value[1] $value[2]; $value[3] Группа</option>";	
+				    }	   	 		   		
+				?>
+			</select>
+
+			<?
+				$query ="SELECT * FROM subjects;";
+				$result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link)); 
+				foreach ($result as $key => $value) {
+					$subjects[$key][0] = $value['id'];
+					$subjects[$key][1] = $value['name'];
+				}
+
+			?>
+			<select name="subject">	
+				<?
+				    foreach ($subjects as $key => $value) {
+				    	echo "<option value='$value[0]'>$value[1]</option>";	
+				    }	   	 		   		
+				?>
+			</select>
+
+			<p>Оценка
+			<input type="number" name="grade" min="1" max="5" required>
+			Дата
+			<input type="data" name="date" required>	
+			</p>
+			<p><input type="submit" value="Занести запись"></p>
+		</div>
+	</form>	
+
+	
+
+	<form method="GET" action="tableSubject.php">
+		<div class="grade">
+			<p style="text-align: center;">Формы на вывод в таблицу</p>
+			<select name="subject">	
+					<?
+					    foreach ($subjects as $key => $value) {
+					    	echo "<option value='$value[0]'>$value[1]</option>";	
+					    }	   	 		   		
+					?>
+			</select>
+
+			<select name="student">	
+					<?										
+						$students_unique = array_unique($group);
+					    foreach ($students_unique as $key => $value) {
+					    	echo "<option value='$value'>$value Группа</option>";	
+					    }	   	 		   		
+					?>
+			</select>
+			<input type="submit" value="Показать студентов">
+		</div>
+
+	</form>
+
+	<form method="GET" action="tableOneStudent.php">
+		<div class="grade">
+			<select name="student">	
+					<?										
+						$students_unique = array_unique($student);
+					    foreach ($students_unique as $key => $value) {
+					    	echo "<option value='$value'>$value</option>";	
+					    }	   	 		   		
+					?>
+			</select>
+			Начало
+			<input type="data" name="date" required>
+			Конец
+			<input type="data" name="date" required>
+			<p><input type="submit" value="Показать оценки"></p>
+		</div>
+
+	</form>
+
+</body>
+<style type="text/css">	
+
+</style>
+</html>
---
+++ tableOneStudent.php
@@ -0,0 +1,115 @@
+<?	
+	if (isset($_GET['student'])){
+	   setcookie("student", $_GET['student'], time()+600);
+	   $student = $_GET['student'];	
+
+	}else{
+	   $student = $_COOKIE['student'];	
+	}
+
+	if (isset($_GET['subject'])){
+	   setcookie("subject", $_GET['subject'], time()+600);
+	   $subject = $_GET['subject'];	
+
+	}else{
+	   $subject = $_COOKIE['subject'];	
+	}
+
+	if (isset($_GET['colum'])){
+	   setcookie("colum", $_GET['colum'], time()+600);
+	   $colum = $_GET['colum'];	
+
+	}else{
+	   $colum = $_COOKIE['colum'];	
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="/style.css">
+</head>
+<?
+	$columsName = array('Имя','Фамилия','Оценка', 'Дата');
+	$numberToColum = array(
+							'0' => 'students.first_name',
+							'1' => 'students.second_name',
+							'2'	=> 'grades.grade',
+							'3' => 'grades.data');
+
+	$link = mysqli_connect('localhost', 'root', 'root','libraryweb') 
+	    or die("Ошибка " . mysqli_error($link));
+
+
+	if(isset($_GET['sort'])){
+		$sorting = "ORDER BY     
+					{$numberToColum[$colum]} {$_GET['sort']}";
+	}
+
+
+	$query ="SELECT students.first_name, students.second_name, 
+					subjects.name, grades.grade, grades.data
+			FROM 
+					libraryweb.grades
+			JOIN  
+					libraryweb.students
+				ON 
+					students.group = $student AND grades.id_student = students.id
+			JOIN  libraryweb.subjects
+				ON subjects.id = $subject AND grades.id_subject = subjects.id
+			$sorting";
+
+	$nextSort = (($_GET['sort'] == "ASC") ? "DESC" : "ASC");
+					
+	echo $query;				
+	$result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link));
+
+	foreach ($result as $key => $value) {
+		$grades_st[$key][0] = $value['first_name'];
+		$grades_st[$key][1] = $value['second_name'];		
+		$grades_st[$key][2] = $value['grade'];
+		$grades_st[$key][3] = $value['data'];
+		$subject = $value['name'];
+	}  
+?>
+
+
+<body> 
+	<?echo "Результаты по: $subject $student группы";?>
+	<table class='table'>
+		<thead id="tHead">
+			<tr>
+				<?
+					foreach ($columsName as $key => $value) {
+						echo "<th><a href='tableSubject.php?colum=$key&sort=$nextSort'>$value</a></th>";
+					}
+				?>			
+			</tr>
+		
+		</thead>
+		<tbody id="tBody">
+		<tr>
+			<?
+			if($grades_st){
+				foreach ($grades_st as $line) {
+					echo "<tr>";
+					foreach ($line as $line_split_values) {
+							echo "<td>".$line_split_values."</td>";
+					}
+					echo "</tr>";
+				}
+			}
+			?>	
+		</tr>
+
+		</tbody>
+
+	</table>
+	<a href="index.php">Форма выбора</a>
+</body>
+<style type="text/css">	
+
+</style>
+</html>
